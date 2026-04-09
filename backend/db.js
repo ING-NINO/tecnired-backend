217f92abac15232acc37ea108caa02c70cab859f
Juan Camilo Sanchez <cs7256081@gmail.com>
2026-04-09 15:29:21 -0500

    conexion a railway

--- backend/db.js
+++ backend/db.js
@@ -1,18 +1,19 @@
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "123456789",
-  database: "soporte_usuarios",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT, // 👈 IMPORTANTE
 });
 
 db.connect((err) => {
   if (err) {
-    console.log("Error al conectar:", err);
-    return;
+    console.log("❌ Error conexión DB:", err);
+  } else {
+    console.log("✅ MySQL conectado");
   }
-  console.log("Conectado a MySQL");
 });
 
 module.exports = db;

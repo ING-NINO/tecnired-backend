7481fd6f6e3b3b84ec6a4c06fd5ba0d88678c720
Juan Camilo Sanchez <cs7256081@gmail.com>
2026-04-09 18:01:44 -0500

    fix smtp

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,9 @@ const PORT = process.env.PORT || 3000;
 
 // ===== CONFIGURACIÓN CORREO =====
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
